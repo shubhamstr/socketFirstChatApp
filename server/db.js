4fde4e00b6638edbe21a9d690dae8256e5ea1330
shubhamvinnovate <shubham.sutar@vinnovatetechnologies.com>
2024-05-25 21:13:34 +0530

    fix(APIs): update server code

---
+++ server/db.js
@@ -0,0 +1,14 @@
+var mysql = require("mysql")
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "socket_chat",
+})
+
+db.connect(function (err) {
+  if (err) throw err
+  console.log("Connected!")
+})
+
+export default db
--- server/server.js
+++ server/server.js
@@ -1,34 +1,36 @@
-const express = require('express');
-const app = express();
-const http = require('http').createServer(app);
+const express = require("express")
+const app = express()
+const http = require("http").createServer(app)
 
-const PORT = process.env.PORT || 5000;
+const db = require("./db")
 
+const PORT = process.env.PORT || 5000
 
 http.listen(PORT, () => {
-    console.log(`listening on http://localhost:${PORT}`);
+  console.log(`listening on http://localhost:${PORT}`)
 })
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + "/public"))
 
-app.get('/', (req, res) => {
-    // res.send('hello world');
-    res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+  res.send("server is running on " + PORT)
 })
 
+app.get("/html", (req, res) => {
+  res.sendFile(__dirname + "/index.html")
+})
 
 // socket code
-
-const io = require('socket.io')(http)
+const io = require("socket.io")(http)
 io.on("connection", (socket) => {
-    console.log('connected..');
+  console.log("connected..")
 
-    socket.on("connected", (username) => {
-        socket.broadcast.emit("connected", username);
-    })
+  socket.on("connected", (username) => {
+    socket.broadcast.emit("connected", username)
+  })
 
-    socket.on("message", (msg) => {
-        // console.log(msg);
-        socket.broadcast.emit("message", msg);
-    })
+  socket.on("message", (msg) => {
+    // console.log(msg);
+    socket.broadcast.emit("message", msg)
+  })
 })
