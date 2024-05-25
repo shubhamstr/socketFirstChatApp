0c4130255ea0b8d72b0bcac6a5ca90d1019c510d
shubhamvinnovate <shubham.sutar@vinnovatetechnologies.com>
2024-05-25 23:20:14 +0530

    fix(APIs): users get insert API create

--- server/db/db.js
+++ server/db/db.js
@@ -8,7 +8,7 @@ const db = mysql.createConnection({
 
 db.connect(function (err) {
   if (err) throw err
-  console.log("Connected!")
+  console.log("Connected to DB!")
 })
 
 module.exports = db
--- server/routes/users.js
+++ server/routes/users.js
@@ -4,13 +4,36 @@ const db = require("../db/db")
 
 // middleware that is specific to this router
 const timeLog = (req, res, next) => {
-  console.log("Time: ", Date.now())
+  console.log("Time: ", Date.now().toLocaleString())
   next()
 }
 router.use(timeLog)
 
 router.get("/get-all", (req, res) => {
-  res.send("About birds")
+  db.query("SELECT * FROM users", function (err, result, fields) {
+    if (err) throw err
+    console.log(result)
+    res.send({
+      err: false,
+      msg: "Users Fetched Successfully!!",
+      data: result,
+    })
+  })
+})
+
+router.post("/insert", (req, res) => {
+  if (req.body.signUpType === "guest") {
+    var sql = `INSERT INTO users (username) VALUES (${req.body.userName})`
+  }
+  db.query(sql, function (err, result) {
+    if (err) throw err
+    console.log(result)
+    res.send({
+      err: false,
+      msg: "User Inserted Successfully!!",
+      data: result,
+    })
+  })
 })
 
 module.exports = router
