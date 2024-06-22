932ca74120952b46c6aef5cc70091bffb761528a
shubhamvinnovate <shubham.sutar@vinnovatetechnologies.com>
2024-06-22 18:31:07 +0530

    update notification API integration

--- client/src/views/Settings/components/Notifications/Notifications.js
+++ client/src/views/Settings/components/Notifications/Notifications.js
@@ -1,7 +1,9 @@
+// eslint-disable-next-line no-unused-vars
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/styles';
+import { setDetails } from '../../../../store/authSlice';
 import {
   Card,
   CardHeader,
@@ -14,7 +16,8 @@ import {
   Typography,
   Button
 } from '@material-ui/core';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { updateNotificationApi } from '../../../../api/users';
 
 const useStyles = makeStyles(() => ({
   root: {},
@@ -26,24 +29,47 @@ const useStyles = makeStyles(() => ({
 
 const Notifications = props => {
   const { className, ...rest } = props;
-
-  const [user, setUser] = useState({});
-
-  const classes = useStyles();
-
+  const dispatch = useDispatch();
   const auth = useSelector(state => state.auth);
   const { userDetails } = auth;
 
-  useEffect(() => {
-    // eslint-disable-next-line no-console
-    // console.log(userDetails);
-    setUser({
-      email_notification_flag: userDetails.email_notification_flag,
-      push_notification_flag: userDetails.push_notification_flag,
-      email_message_flag: userDetails.email_message_flag,
-      push_message_flag: userDetails.push_message_flag
+  const [user, setUser] = useState(userDetails);
+
+  const classes = useStyles();
+
+  const handleChange = e => {
+    // console.log(e.target.name, e.target.checked);
+    setUser(prev => {
+      return { ...prev, [e.target.name]: e.target.checked ? 1 : 0 };
     });
-  }, [userDetails]);
+  };
+
+  const handleSave = async () => {
+    // console.log(user);
+    const resp = await updateNotificationApi({
+      id: user.id,
+      email_notification_flag: user.email_notification_flag,
+      push_notification_flag: user.push_notification_flag,
+      email_message_flag: user.email_message_flag,
+      push_message_flag: user.push_message_flag
+    });
+    if (resp.data.err) {
+      alert(resp.data.msg);
+    } else {
+      dispatch(
+        setDetails({
+          type: 'userDetails',
+          value: user
+        })
+      );
+      alert(resp.data.msg);
+    }
+  };
+
+  // useEffect(() => {
+  //   console.log(userDetails);
+  //   console.log(user);
+  // }, []);
 
   return (
     <Card {...rest} className={clsx(classes.root, className)}>
@@ -67,6 +93,8 @@ const Notifications = props => {
                   />
                 }
                 label="Email"
+                name="email_notification_flag"
+                onChange={handleChange}
               />
               <FormControlLabel
                 control={
@@ -76,6 +104,8 @@ const Notifications = props => {
                   />
                 }
                 label="Push Notifications"
+                name="push_notification_flag"
+                onChange={handleChange}
               />
               {/* <FormControlLabel
                 control={<Checkbox color="primary" />}
@@ -103,6 +133,8 @@ const Notifications = props => {
                   />
                 }
                 label="Email"
+                name="email_message_flag"
+                onChange={handleChange}
               />
               <FormControlLabel
                 control={
@@ -112,6 +144,8 @@ const Notifications = props => {
                   />
                 }
                 label="Push Notifications"
+                name="push_message_flag"
+                onChange={handleChange}
               />
               {/* <FormControlLabel
                 control={
@@ -127,7 +161,7 @@ const Notifications = props => {
         </CardContent>
         <Divider />
         <CardActions>
-          <Button color="primary" variant="outlined">
+          <Button color="primary" onClick={handleSave} variant="outlined">
             Save
           </Button>
         </CardActions>
