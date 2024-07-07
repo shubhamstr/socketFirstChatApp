a4750be101f1596451f6ec827f0e1488427336f1
shubhamvinnovate <shubham.sutar@vinnovatetechnologies.com>
2024-07-08 00:31:13 +0530

    fix issue

--- client/src/views/Chat/components/ChatScreen/ChatScreen.js
+++ client/src/views/Chat/components/ChatScreen/ChatScreen.js
@@ -113,7 +113,6 @@ const ChatScreen = () => {
                 <Conversation
                   info="Yes i can do it for you"
                   key={index}
-                  lastSenderName="Lilly"
                   name={user.username}
                   onClick={() => handleChat(user)}>
                   <Avatar
@@ -124,78 +123,6 @@ const ChatScreen = () => {
                 </Conversation>
               );
             })}
-          {/* <Conversation
-            info="Yes i can do it for you"
-            lastSenderName="Lilly"
-            name="Lilly">
-            <Avatar
-              name="Lilly"
-              src="https://chatscope.io/storybook/react/assets/lilly-aj6lnGPk.svg"
-            />
-          </Conversation>
-          <Conversation
-            info="Yes i can do it for you"
-            lastSenderName="Joe"
-            name="Joe">
-            <Avatar
-              name="Joe"
-              src="https://chatscope.io/storybook/react/assets/joe-v8Vy3KOS.svg"
-            />
-          </Conversation>
-          <Conversation
-            info="Yes i can do it for you"
-            lastSenderName="Emily"
-            name="Emily">
-            <Avatar
-              name="Emily"
-              src="https://chatscope.io/storybook/react/assets/emily-xzL8sDL2.svg"
-            />
-          </Conversation>
-          <Conversation
-            info="Yes i can do it for you"
-            lastSenderName="Kai"
-            name="Kai">
-            <Avatar
-              name="Kai"
-              src="https://chatscope.io/storybook/react/assets/kai-5wHRJGb2.svg"
-            />
-          </Conversation>
-          <Conversation
-            info="Yes i can do it for you"
-            lastSenderName="Akane"
-            name="Akane">
-            <Avatar
-              name="Akane"
-              src="https://chatscope.io/storybook/react/assets/akane-MXhWvx63.svg"
-            />
-          </Conversation>
-          <Conversation
-            info="Yes i can do it for you"
-            lastSenderName="Eliot"
-            name="Eliot">
-            <Avatar
-              name="Eliot"
-              src="https://chatscope.io/storybook/react/assets/eliot-JNkqSAth.svg"
-            />
-          </Conversation>
-          <Conversation
-            info="Yes i can do it for you"
-            lastSenderName="Zoe"
-            name="Zoe">
-            <Avatar
-              name="Zoe"
-              src="https://chatscope.io/storybook/react/assets/zoe-E7ZdmXF0.svg"
-            />
-          </Conversation>
-          <Conversation
-            info="Yes i can do it for you"
-            lastSenderName="Patrik"
-            name="Patrik">
-            <Avatar
-              name="Patrik"
-              src="https://chatscope.io/storybook/react/assets/patrik-yC7svbAR.svg"
-            />
-          </Conversation> */}
         </ConversationList>
 
         {Object.keys(selectedChat).length > 0 ? (
