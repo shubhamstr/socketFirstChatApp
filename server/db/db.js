f9c7584669c04be63d778d2f5e8bfa02d0478fd3
shubhamvinnovate <shubham.sutar@vinnovatetechnologies.com>
2024-07-09 01:45:03 +0530

    db env fix

--- server/db/db.js
+++ server/db/db.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 console.log(process.env)
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
